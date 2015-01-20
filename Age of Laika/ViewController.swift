3dcc160d1d50d58ff6849d6b5f401b6482bf6d45
personary <apersonary@gmail.com>
2015-01-19 22:08:14 -0500

    second commit

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -32,5 +32,23 @@ class ViewController: UIViewController {
         humanYearsTextField.resignFirstResponder()
     }
 
+    @IBAction func convertToRealDogYearsButtonPressed(sender: UIButton) {
+        dogYearsLabel.hidden = false
+        let humanYearsDouble = Double((humanYearsTextField.text as NSString).doubleValue)
+        
+        if humanYearsDouble <= 2 {
+            dogYearsLabel.text = "\(Double(humanYearsDouble) * 10.5) in Real Dog Years"
+        } else if humanYearsDouble > 2 {
+            let firstTwoYears = 2 * 10.5
+            let remainingYears = humanYearsDouble - 2
+            let dogYears = firstTwoYears + Double(remainingYears * 4)
+            dogYearsLabel.text = "\(dogYears) in Real Dog Years"
+        } else {
+            dogYearsLabel.text = "0 in Real Dog Years"
+        }
+        
+        humanYearsTextField.text = ""
+        humanYearsTextField.resignFirstResponder()
+    }
 }
 
